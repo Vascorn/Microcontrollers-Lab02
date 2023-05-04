4f83c51b8e35cc2dfca1cd8643d1dd47ea3d3034
Vascorn <vasilikirn@gapps.auth.gr>
2023-05-04 13:26:20 +0300

    final

--- main.c
+++ main.c
@@ -2,40 +2,50 @@
 #include <stdlib.h>
 #include "uart.h"
 #include "gpio.h"
+#include "leds.h"
 
-
-#define P_LED P_LED_G
+#define P_LED P_LED_R
 
 static int counter;
 static uint8_t last_digit_AEM;
 
 void switch_ISR(int status){
-	if (status & 1 << GET_PIN_INDEX(P_SW)){
-		char temp[10];
+		char temp[100];
 		gpio_toggle(P_LED);
+		
 		counter++;
-		sprintf(temp, "%d \r\n", counter);
+	
+		if (gpio_get(P_LED))
+			sprintf(temp, "\r\nButton pressed, Led is on. Counter = %d \r\n", counter);
+		else
+			sprintf(temp, "\r\nButton pressed, Led is off. Counter = %d \r\n", counter);
 		uart_print(temp);
-	}
+		uart_print("Insert your AEM \r\n");
 }
 
-void led_ISR(int status){
-	if (status & 1 << GET_PIN_INDEX(P_LED)){
-		const char* temp = gpio_get(P_LED) ? "The led is now on!": "The led is now off!";
-		uart_print(temp);
-	}
-}
+
 
 void getchar_ISR(uint8_t c){
-	if (c != '\r')
+	
+	
+	if (c != '\r'){
 		last_digit_AEM = c;
+	  uart_tx(c);
+	}
 	else{
+		uart_print("\r\n");
 		gpio_set(P_LED, last_digit_AEM % 2);
+		if (gpio_get(P_LED))
+			uart_print("Odd AEM, Led is on\r\n");
+		else
+			uart_print("Even AEM, Led is off \r\n");
+		uart_print("Insert your AEM \r\n");
 	}
 		
 }
 
 int main(){
+	__enable_irq();
 	
 	uart_init(115200);
 	uart_enable();
@@ -43,15 +53,16 @@ int main(){
 	uart_set_rx_callback(getchar_ISR);
 	
 	//setting modes to the LED and the button pin
-	gpio_set_mode(P_LED, Output);
-	gpio_set_mode(P_SW, Input);
+	leds_init();
+	
+	gpio_set_mode(P_SW, PullDown);
+	gpio_set_trigger(P_SW,Rising);
 	
 	gpio_set_callback(P_SW, switch_ISR);
-	gpio_set_callback(P_LED,led_ISR);
 	
 	NVIC_SetPriority(EXTI15_10_IRQn, 1);
 	
-	
+
 	while(1){
 			//wait for interrupt
 	}
